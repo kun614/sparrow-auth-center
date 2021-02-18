2c431a2f7bd1717ea6bca44c330a41d9e5001fc6
huk10 <huk10@mingyuanyun.com>
2021-02-18 15:56:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <Login />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Login from "./pages/login.vue"
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Login
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="p-3 border rounded-md shadow-md w-2/5">
+      <form action="" method="post">
+        <div class="m-2">
+          <label for="email">Email address</label>
+          <input type="email" name="email" id="email" class="border p-2 rounded-md" />
+        </div>
+        <div class="m-2">
+          <label for="passwd">Password</label>
+          <input type="password" name="passwd" id="passwd" class="border p-2 rounded-md" />
+        </div>
+        <div class="flex m-2">
+          <div class="">
+            <input type="checkbox" name="remember" id="remember" />
+            <label for="remember">Remember me</label>
+          </div>
+          <div class="color-blue">Forgot your password?</div>
+        </div>
+        <button class="p-2 bg-blue-600 rounded-md" type="submit">Sign in</button>
+      </form>
+    </div>
+  </div>
+</template>
